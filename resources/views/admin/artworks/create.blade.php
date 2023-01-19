6986f7358aebe951e3e879808322f5888e405cb3
Stanghi <stangherlin.matti@gmail.com>
2023-01-19 17:33:25 +0100

    add view artworks

---
+++ resources/views/admin/artworks/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('title')
+    | Admin Project-Create
+@endsection
+
+@section('content')
+    <div class="container">
+
+        @if ($errors->any())
+            <div class="alert alert-danger m-5" role="alert">
+                <h2><i class="fa-solid fa-triangle-exclamation"></i>Error</h2>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('admin.artworks.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name *</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                    placeholder="Add name...">
+                @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="year" class="form-label">Client name *</label>
+                <input type="text" class="form-control @error('year') is-invalid @enderror" id="year" name="year"
+                    placeholder="Add year...">
+                @error('year')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit
+                <i class="fa-solid fa-file-import ms-1"></i>
+            </button>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/admin/artworks/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('title')
+    | Admin Project-Create
+@endsection
+
+@section('content')
+    <div class="container">
+
+        @if ($errors->any())
+            <div class="alert alert-danger m-5" role="alert">
+                <h2><i class="fa-solid fa-triangle-exclamation"></i>Error</h2>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('admin.artworks.update', $artwork) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="name" class="form-label">Name *</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                    placeholder="Add name..." value="{{ old('name', $artwork->name) }}">
+                @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="year" class="form-label">Client name *</label>
+                <input type="text" class="form-control @error('year') is-invalid @enderror" id="year" name="year"
+                    placeholder="Add year..." value="{{ old('title', $artwork->year) }}">
+                @error('year')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit
+                <i class="fa-solid fa-file-import ms-1"></i>
+            </button>
+        </form>
+    </div>
+@endsection
